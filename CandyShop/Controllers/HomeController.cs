0dfefe93037c564b4b21b412d4ecaaf157a9654b
s1me0n4o <s1me0n4o@abv.bg>
2020-01-18 18:40:17 +0200

    adding partition view and HomeController, carousel

---
+++ CandyShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Models;
+using CandyShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CandyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepo _productRepo;
+
+        public HomeController(IProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                ProductsOfTheWeek = _productRepo.ProductOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ CandyShop/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CandyShop.Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Products> ProductsOfTheWeek  { get; set; }
+    }
+}
